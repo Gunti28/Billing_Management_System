e4e2e5f84bb9af7ed963e08a00009cf6b8e0e9bf
Patan Abubakar <patan.abubakar.2002@gmail.com>
2025-03-06 15:59:08 +0530

    Update ICategoryService.java

--- src/main/java/com/srnrit/BMS/service/ICategoryService.java
+++ src/main/java/com/srnrit/BMS/service/ICategoryService.java
@@ -1,12 +1,5 @@
 package com.srnrit.BMS.service;
 
-import java.util.List;
-
-import com.srnrit.BMS.dto.CategoryRequestDTO;
-import com.srnrit.BMS.dto.CategoryResponseDTO;
-
-import java.util.List;
-
 
 public interface ICategoryService 
 {
