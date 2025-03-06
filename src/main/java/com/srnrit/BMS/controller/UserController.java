462c30fd3ef1a11e56b75d098950205daf1b79e0
Sujit Maharana <sujitmaharana1111@gmail.com>
2025-03-06 17:37:16 +0530

    added deleteUserByID api and adding some exception handler method in GlobalExceptionHandler class

--- src/main/java/com/srnrit/BMS/controller/UserController.java
+++ src/main/java/com/srnrit/BMS/controller/UserController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.srnrit.BMS.dto.UserRequestDTO;
 import com.srnrit.BMS.dto.UserResponseDTO;
@@ -28,15 +32,35 @@ public class UserController {
 			     produces = {"application/json"})
 	public ResponseEntity<?> createUser(@Valid @RequestBody UserRequestDTO dto)
 	{
-		UserResponseDTO responseDTO = userService.saveUser(dto);
+		UserResponseDTO responseDTO = this.userService.saveUser(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
 	}
 	
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "/{userId}")
     public ResponseEntity<?> deleteUserById(@PathVariable String userId)
     {
-    	System.out.println(userId);
-    	return ResponseEntity.status(HttpStatus.OK).body("user deleted successfully");
+    	String msg = this.userService.deleteUserById(userId);
+    	return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
+	
+	/*
+	@PutMapping("/editProfileImage/{userid}")
+	public ResponseEntity<UserResponseDTO> editProfileImage(@RequestParam MultipartFile file,
+			                                        @PathVariable("userid") String userId
+			)
+	{
+		 UserResponseDTO userResponseDTO = this.userService.editProfileImage(file,userId);
+		 
+		 return new ResponseEntity<UserResponseDTO>(userResponseDTO,HttpStatus.OK);
+	}
+	*/
+	
+	@GetMapping(value = "/get/{userId}")
+	public ResponseEntity<?> getUserByUserId(@PathVariable String userId)
+	{
+		
+		return ResponseEntity.status(HttpStatus.FOUND).body("UserFound");
+	}
+	
 	
 }
