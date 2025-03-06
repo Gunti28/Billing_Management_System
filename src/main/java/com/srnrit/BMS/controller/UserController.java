283911cf58d9d32a11f8bd2984528005b986c662
ushasripalle <ushasripalle@srnritsolutions.com>
2025-03-06 16:56:35 +0530

    Commited SuccesFully

--- src/main/java/com/srnrit/BMS/controller/UserController.java
+++ src/main/java/com/srnrit/BMS/controller/UserController.java
@@ -3,7 +3,9 @@ package com.srnrit.BMS.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,3 +34,4 @@ public class UserController {
 	
 
 }
+
--- src/test/java/com/srnrit/BMS/BmsApplicationTests.java
+++ src/test/java/com/srnrit/BMS/BmsApplicationTests.java
@@ -1,24 +1,11 @@
 package com.srnrit.BMS;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Optional;
-
-import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.srnrit.BMS.dao.UserDao;
-import com.srnrit.BMS.dto.UserRequestDTO;
-import com.srnrit.BMS.dto.UserResponseDTO;
-import com.srnrit.BMS.entity.User;
-
 import com.srnrit.BMS.repository.UserRepository;
-
-import com.srnrit.BMS.service.UserService;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -31,72 +18,7 @@ class BmsApplicationTests {
 	@Mock
 	UserRepository userRepository ;
 	
-	
-//	@Autowired
-//	UserDao userDAO;
-
-//	@Test
-//	void userSaveTest() 
-//	{
-//		User user = new User();
-//		user.setUserName("venu");
-//		user.setUserEmail("venu@gmail.com");
-//		user.setUserPassword("Venu@!2334");
-//		user.setUserGender("Male");
-//		user.setUserPhone(8712339224L);
-//		user.setTermsAndConditions(true);
-//		
-//		Optional<User> saveuser = userDAO.saveuser(user);
-//		 assertTrue(saveuser.isPresent()); 
-//		 System.out.println(saveuser.get());
-//		
-//		
-//	}
-	
-	@Autowired
-	UserService service;
-	
-//	@Test
-//	void saveUserService()
-//	{
-//		UserRequestDTO user = new UserRequestDTO();
-//		user.setUserName("venu");
-//		user.setUserEmail("venu@gmail.com");
-//		user.setUserPassword("Venu@!2334");
-//		user.setUserGender("Male");
-//		user.setUserPhone("8712339224");
-//		user.setTermsAndConditions(true);
-//		UserResponseDTO saveUser = service.saveUser(user);
-//		System.out.println(saveUser);
-//	}
-	
-	
-	//service layer unit testing
-	@Test
-	void deleteUserById()
-	{
-
-		User user = new User();
-		user.setUserName("venu");
-		user.setUserEmail("venu@gmail.com");
-		user.setUserPassword("Venu@!2334");
-		user.setUserGender("Male");
-		user.setUserPhone(8712339224L);
-		user.setTermsAndConditions(true);
-		
-		Optional<User> saveuser = userDAO.saveuser(user);
-		 assertTrue(saveuser.isPresent()); 
-		 System.out.println(saveuser.get());	
-	}
-}	
-	
-	
-
-
-		String msg = service.deleteUserById("Uid_01");
-		System.out.println(msg);
-	}
-	
+}
 	
 
 
