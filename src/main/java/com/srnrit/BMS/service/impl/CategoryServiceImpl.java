f984526918c0b86683b3743c2a0431bb552b11db
Naga Vethendra <vethendrapattamsetti@gmail.com>
2025-03-07 09:55:50 +0530

    Added some functionality

--- src/main/java/com/srnrit/BMS/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/srnrit/BMS/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,73 @@
 package com.srnrit.BMS.service.impl;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.srnrit.BMS.dto.CategoryRequestDTO;
+import com.srnrit.BMS.dto.CategoryResponseDTO;
+import com.srnrit.BMS.dto.ProductRequestDTO;
+import com.srnrit.BMS.entity.Category;
+import com.srnrit.BMS.entity.Product;
 import com.srnrit.BMS.service.ICategoryService;
 
-public class CategoryServiceImpl implements ICategoryService {
+@Service
+public class CategoryServiceImpl implements ICategoryService 
+{
+	private ICategoryService iCategoryService;
+
+	public CategoryServiceImpl(ICategoryService iCategoryService) {
+		super();
+		this.iCategoryService = iCategoryService;
+	}
+
+	@Override
+	public CategoryResponseDTO addCategoryWithProducts(CategoryRequestDTO categoryRequestDTO) {
+		Category category=new Category();
+		category.setCategoryname(categoryRequestDTO.getCategoryName());
+
+		if(categoryRequestDTO.getProducts()!=null)
+		{
+			
+			
+			/*
+			 * List<Product> products = categoryRequestDTO.getProducts().stream()
+			 * .map(productRequest -> new Product( productRequest.getProductName(),
+			 * productRequest.getProductImage(), productRequest.getProductPrice(),
+			 * productRequest.getProductQuantity(), productRequest.getInStock() ))
+			 * .collect(Collectors.toList());
+			 * 
+			 * products.stream().forEach(product -> category.addProduct(product));
+			 */
+		}
+		else 
+		{
+            
+		}
+		return null;
+	}
+
+	@Override
+	public List<CategoryResponseDTO> getAllCategory() 
+	{
+
+		return null;
+	}
+
+	@Override
+	public String updateCategory(String categoryId, String categoryName) 
+	{
+
+		return null;
+	}
+
+	@Override
+	public CategoryResponseDTO findCategoryByCategoryId(String categoryId) 
+	{
+
+		return null;
+	}
 
 }
