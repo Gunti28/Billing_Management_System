0dbf97355e0e48d874465a22f47c6f88d5f04611
venu kumar <k.venukumar6543@gmail.com>
2025-03-07 14:50:21 +0530

    added Edit Image method in UserServiceImpl class

--- src/main/java/com/srnrit/BMS/service/UserService.java
+++ src/main/java/com/srnrit/BMS/service/UserService.java
@@ -1,5 +1,7 @@
 package com.srnrit.BMS.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.srnrit.BMS.dto.LoginRequestDTO;
 import com.srnrit.BMS.dto.UserRequestDTO;
 import com.srnrit.BMS.dto.UserResponseDTO;
@@ -10,4 +12,5 @@ public interface UserService {
 	UserResponseDTO updateUserById(UserRequestDTO userRequestDTO, String userId);
 	UserResponseDTO findUserById(String userId);
 	UserResponseDTO loginUserByEmailAndPassword(String email,String password);
+	UserResponseDTO editUserImage(MultipartFile file,String userId);
 }
