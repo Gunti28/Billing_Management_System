40039ae9fa0466cbf8893e2003bb364a45fccda6
Naga Vethendra <vethendrapattamsetti@gmail.com>
2025-03-07 15:12:14 +0530

    Implemented global exceptions

--- src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
+++ src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
@@ -1,5 +1,25 @@
 package com.srnrit.BMS.globalexcepiton;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.srnrit.BMS.dto.Message;
+import com.srnrit.BMS.exception.categoryexceptions.CategoryNotCreatedException;
+import com.srnrit.BMS.exception.categoryexceptions.CategoryNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler 
+{
+	@ExceptionHandler(exception = CategoryNotCreatedException.class)
+    public ResponseEntity<?> categoryNotCreatedException(CategoryNotCreatedException e)
+    {
+   	 return new ResponseEntity<Message>( new  Message(e.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+	@ExceptionHandler(exception = CategoryNotFoundException.class)
+   public ResponseEntity<?> categoryNotFoundException(CategoryNotFoundException e)
+   {
+  	 return new ResponseEntity<Message>( new  Message(e.getMessage()),HttpStatus.NOT_FOUND);
+   }
 }
