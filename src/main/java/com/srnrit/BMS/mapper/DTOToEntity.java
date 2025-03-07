e5c4e288cd12a886203fc5af6b22b87d02219f89
Naga Vethendra <vethendrapattamsetti@gmail.com>
2025-03-07 14:48:09 +0530

    Implemented Service class

--- src/main/java/com/srnrit/BMS/mapper/DTOToEntity.java
+++ src/main/java/com/srnrit/BMS/mapper/DTOToEntity.java
@@ -18,10 +18,6 @@ public class DTOToEntity
 		return category;		
 	}
 
-
-
-
-	
 	public static Product toProduct(ProductRequestDTO productRequestDTO) {
 		return new Product(
 				productRequestDTO.getProductName(),
