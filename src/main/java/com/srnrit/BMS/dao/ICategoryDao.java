7c8b444fcf557c7d921e59dd7dd7b00bac55118d
Poojitha73 <poojithavarma2002@gmail.com>
2025-03-10 10:53:50 +0530

    Some changes

--- src/main/java/com/srnrit/BMS/dao/ICategoryDao.java
+++ src/main/java/com/srnrit/BMS/dao/ICategoryDao.java
@@ -8,8 +8,14 @@ import com.srnrit.BMS.entity.Category;
 public interface ICategoryDao 
 {
     Optional<Category> insertCategory(Category category);
+    
     Optional<List<Category>> getAllCategory();
+    
 	Optional<String> updateCategory(String categoryId,String categoryName);
+	
 	Optional<Category> getCategoryByCategoryId(String categoryId) ;
+	
 	Optional<String> deleteCategory(String categoryId); 
 }
+
+
