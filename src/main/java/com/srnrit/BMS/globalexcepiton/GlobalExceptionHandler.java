4b76564fb8a9ec3152f7ce3fcb14bb9ad005e085
Tarun Varma <138118412+Tarunvarmalikith@users.noreply.github.com>
2025-03-10 17:47:42 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
+++ src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package com.srnrit.BMS.globalexcepiton;
 
 import java.util.HashMap;
 import java.util.Map;
-<<<<<<< HEAD
+
 
 import org.apache.logging.log4j.message.Message;
 import org.springframework.http.HttpStatus;
@@ -10,6 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.srnrit.BMS.dto.Message;
+import com.srnrit.BMS.exception.categoryexceptions.CategoryNotCreatedException;
+import com.srnrit.BMS.exception.categoryexceptions.CategoryNotFoundException;
+import org.springframework.validation.FieldError;
 
 import com.srnrit.BMS.exception.productexceptions.ProductNotCreatedException;
 
@@ -35,40 +41,6 @@ public class GlobalExceptionHandler {
 //    	
 //    	return new ResponseEntity<Message>(new Message(e.getMessage()),HttpStatus.BAD_REQUEST);    
 //    	}
-=======
-
-
-import org.apache.logging.log4j.message.Message;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import com.srnrit.BMS.dto.Message;
-import com.srnrit.BMS.exception.categoryexceptions.CategoryNotCreatedException;
-import com.srnrit.BMS.exception.categoryexceptions.CategoryNotFoundException;
-import org.springframework.validation.FieldError;
-
-import com.srnrit.BMS.exception.productexceptions.ProductNotCreatedException;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-  
-   // Handle Validation Errors and Return Only Messages
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-
-        // Extract only the field and its corresponding error message
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    	
-    }
   
 	@ExceptionHandler(exception = CategoryNotCreatedException.class)
     public ResponseEntity<?> categoryNotCreatedException(CategoryNotCreatedException e)
@@ -91,5 +63,4 @@ public class GlobalExceptionHandler {
 	  ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST); 
 	  }
 	
->>>>>>> 7310227eab35dca7c21368dda96b10ccc685ec5b
 }
