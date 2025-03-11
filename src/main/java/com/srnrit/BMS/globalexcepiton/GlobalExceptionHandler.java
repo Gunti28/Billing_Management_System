b0c5238815faed93a28330489cc18e154bbfbf8f
harshu808-debug <nagaharshiniprathipati@srnritsolutions.com>
2025-03-11 10:03:16 +0530

    Added validation for duplicate product names

--- src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
+++ src/main/java/com/srnrit/BMS/globalexcepiton/GlobalExceptionHandler.java
@@ -1,17 +1,13 @@
 package com.srnrit.BMS.globalexcepiton;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,8 +24,10 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+    // Handle IllegalArgumentException and return only the message
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
 }
-    
-
-	
-
