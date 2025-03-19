5c517d2f21c14dde271f17f0c48ee93160666acf
Sujit Maharana <sujitmaharana1111@gmail.com>
2025-03-19 11:24:22 +0530

    deleted UserIdGenerator class and we add custome generator in the entity class

--- src/test/java/com/srnrit/BMS/controllertest/UserControllerTest.java
+++ src/test/java/com/srnrit/BMS/controllertest/UserControllerTest.java
@@ -33,10 +33,12 @@ import com.srnrit.BMS.controller.UserController;
 import com.srnrit.BMS.dto.LoginRequestDTO;
 import com.srnrit.BMS.dto.UserRequestDTO;
 import com.srnrit.BMS.dto.UserResponseDTO;
+import com.srnrit.BMS.dto.VerifyOTPRequestDTO;
 import com.srnrit.BMS.entity.User;
 import com.srnrit.BMS.exception.userexceptions.UserNotFoundException;
 import com.srnrit.BMS.globalexcepiton.GlobalExceptionHandler;
 import com.srnrit.BMS.service.UserService;
+import com.srnrit.BMS.util.Message;
 
 @ExtendWith(MockitoExtension.class) // Enables Mockito
 @WebMvcTest(UserController.class)
@@ -548,5 +550,30 @@ public class UserControllerTest {
 		System.err.println(result.getResponse().getContentAsString());
 	}
 
-	//17.
+	//17.verifyOTP api (posative scenario)
+	@Test
+	void testVerifyOTP() throws Exception
+	{
+		System.out.println("UserControllerTest.testVerifyOTP()");
+		 VerifyOTPRequestDTO verifyOTPRequestDTO = new VerifyOTPRequestDTO();
+		 verifyOTPRequestDTO.setEmail("sujitmaharana1111@gmail.com");
+		 verifyOTPRequestDTO.setOtp("1234");
+		 when(this.userService.verifyOTP(any(VerifyOTPRequestDTO.class))).thenReturn(new Message("OTP verified successfully"));
+	     
+		 String verifyOTPJSON = this.objectMapper.writeValueAsString(verifyOTPRequestDTO);
+		 
+		 
+	     MvcResult result = mockMvc.perform(
+	    		        post("/user/VerifyOTP")
+	    		        .contentType(MediaType.APPLICATION_JSON)
+	    		        .content(verifyOTPJSON)
+	    		        )
+	                   .andExpect(status().isOk())
+		               .andExpect(jsonPath("$.message").value("OTP verified successfully"))
+			           .andReturn();
+
+	     System.err.println("Status : " + result.getResponse().getStatus());
+		 System.err.println(result.getResponse().getContentAsString());
+	
+	}
 }
